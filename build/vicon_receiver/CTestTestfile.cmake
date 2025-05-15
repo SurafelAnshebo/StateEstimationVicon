edc5eab138acb022e273b122d1feff55d3877a08
Surafel Anshebo <Surafelrtesfaye@gmail.com>
2025-05-14 22:24:00 -0400

    push vicon_ws just for streaming raw data

---
+++ build/vicon_receiver/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/surafel/vicon_ws/src/ros2-vicon-receiver/vicon_receiver
+# Build directory: /home/surafel/vicon_ws/build/vicon_receiver
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("vicon_receiver__py")
